e13d82ffd2cc7a4bdaf44b39b7b31a57675116b3
Ramiro <GARZA.RAMIROV@GMAIL.COM>
2024-02-26 17:34:59 -0600

    ID retribution corrected, todo fix add to playlist

---
+++ main.py
@@ -0,0 +1,24 @@
+import time
+from modules.scraper import get_songs
+from modules.spotify import add_songs_to_playlist
+
+# Interval (in seconds) for fetching and adding songs
+FETCH_INTERVAL = 3600
+
+
+def main():
+    while True:
+        try:
+            songs = get_songs()
+
+            add_songs_to_playlist(songs)
+
+            print("Songs added to playlist successfully.")
+
+        except Exception as e:
+            print(f"An error occurred: {str(e)}")
+        time.sleep(FETCH_INTERVAL)
+
+
+if __name__ == "__main__":
+    main()
