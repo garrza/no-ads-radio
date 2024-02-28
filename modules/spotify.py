87af88249e66903dd62676fdfed2759f2681c24b
Ramiro <GARZA.RAMIROV@GMAIL.COM>
2024-02-27 20:33:51 -0600

    Fix duplicate error

--- modules/spotify.py
+++ modules/spotify.py
@@ -44,6 +44,7 @@ def get_playlist_tracks():
         for item in playlist["tracks"]["items"]:
             track = item["track"]
             track_ids.append(track["id"])
+            print(track["id"])
     else:
         print("Can't get token for", USERNAME)
 
@@ -54,19 +55,38 @@ def add_songs_to_playlist(songs):
     if TOKEN:
         sp = spotipy.Spotify(auth=TOKEN)
         sp.trace = False
-        song_ids = [get_song_id(artist, song) for artist, song in songs]
 
-        playlist_tracks = get_playlist_tracks()
-        song_ids = [
-            id for id in song_ids if id not in playlist_tracks if id is not None
-        ]
+        # Get the current track IDs in the playlist
+        current_tracks = []
+        offset = 0
+        while True:
+            response = sp.playlist_tracks(PLAYLIST_ID, offset=offset)
+            current_tracks += [item["track"]["id"] for item in response["items"]]
+            offset += len(response["items"])
+            if not response["next"]:
+                break
+
+        song_ids = []
+        for song_info in songs:
+            if len(song_info) == 2:
+                artist, song = song_info
+                song_id = get_song_id(artist, song)
+                if song_id:
+                    if (
+                        song_id not in current_tracks
+                    ):  # Check if the song is not already in the playlist
+                        song_ids.append(song_id)
+                    else:
+                        print(
+                            f"Song '{song}' by '{artist}' is already in the playlist."
+                        )
+                else:
+                    print(f"Song ID not found for {artist} - {song}")
+            else:
+                print(f"Invalid entry: {song_info}")
+
         while song_ids:
             sp.user_playlist_add_tracks(USERNAME, PLAYLIST_ID, song_ids[:100])
             song_ids = song_ids[100:]
-
     else:
         print("Can't get token for", USERNAME)
-        return None
-
-
-get_playlist_tracks()
